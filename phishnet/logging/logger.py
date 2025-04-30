0b0d3ee58a0bbb43a3a0470cfaa4680ffa5b9361
Suman Mondal <suman.mandal1020@gmail.com>
2025-04-30 10:15:44 +0530

    refactor: change log file format

--- phishnet/logging/logger.py
+++ phishnet/logging/logger.py
@@ -1,8 +1,9 @@
-import logging
 import os
+import logging
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+# Log file name DD-MM-YYYY_HH-MM-SS format
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 
 logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
 os.makedirs(logs_path, exist_ok=True)
@@ -10,7 +11,7 @@ os.makedirs(logs_path, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
+    filename = LOG_FILE_PATH,
+    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO
 )
