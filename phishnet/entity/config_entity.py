2d8d3e8afb32fd8a76d259dbf2dec5998a893ca1
Suman Mondal <suman.mandal1020@gmail.com>
2025-05-02 00:33:25 +0530

    style: set indentation make up

--- phishnet/entity/config_entity.py
+++ phishnet/entity/config_entity.py
@@ -17,17 +17,25 @@ class TrainingPipelineConfig:
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
         self.data_ingestion_dir:str=os.path.join(
-            training_pipeline_config.artifact_dir,training_pipeline.DATA_INGESTION_DIR_NAME
+            training_pipeline_config.artifact_dir,
+            training_pipeline.DATA_INGESTION_DIR_NAME
         )
         self.feature_store_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, training_pipeline.FILE_NAME
+                self.data_ingestion_dir, 
+                training_pipeline.DATA_INGESTION_FEATURE_STORE_DIR, 
+                training_pipeline.FILE_NAME
             )
         self.training_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TRAIN_FILE_NAME
+                self.data_ingestion_dir,
+                training_pipeline.DATA_INGESTION_INGESTED_DIR, 
+                training_pipeline.TRAIN_FILE_NAME
             )
         self.testing_file_path: str = os.path.join(
-                self.data_ingestion_dir, training_pipeline.DATA_INGESTION_INGESTED_DIR, training_pipeline.TEST_FILE_NAME
+                self.data_ingestion_dir, 
+                training_pipeline.DATA_INGESTION_INGESTED_DIR, 
+                training_pipeline.TEST_FILE_NAME
             )
+        
         self.train_test_split_ratio: float = training_pipeline.DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO
         self.collection_name: str = training_pipeline.DATA_INGESTION_COLLECTION_NAME
         self.database_name: str = training_pipeline.DATA_INGESTION_DATABASE_NAME
