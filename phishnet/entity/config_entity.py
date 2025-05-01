0c36fd9028dfed82fac976360050574690e0a414
Suman Mondal <suman.mandal1020@gmail.com>
2025-05-02 00:34:43 +0530

    style: indentation make up

--- phishnet/entity/config_entity.py
+++ phishnet/entity/config_entity.py
@@ -16,7 +16,7 @@ class TrainingPipelineConfig:
 
 class DataIngestionConfig:
     def __init__(self,training_pipeline_config:TrainingPipelineConfig):
-        self.data_ingestion_dir:str=os.path.join(
+        self.data_ingestion_dir:str = os.path.join(
             training_pipeline_config.artifact_dir,
             training_pipeline.DATA_INGESTION_DIR_NAME
         )
