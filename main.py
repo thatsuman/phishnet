beae04f5e00d1e37642d25a3a8f421045d364ce8
Suman Mondal <suman.mandal1020@gmail.com>
2025-05-04 02:18:09 +0530

    feat: made data validation component

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import sys
 from phishnet.logging.logger import logging
 from phishnet.exception.exception import PhishnetException
+from phishnet.components.data_validation import DataValidation
 from phishnet.components.data_ingestion import DataIngestion
-from phishnet.entity.config_entity import DataIngestionConfig
+from phishnet.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from phishnet.entity.config_entity import TrainingPipelineConfig
 
 if __name__=='__main__':
     try:
         trainingpipelineconfig = TrainingPipelineConfig()
+        
+        # data ingestion
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
-        
         logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
-
         print(dataingestionartifact)
+        logging.info("Data Initiation Complete")
+
+        # data validation
+        datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
+        data_validation = DataValidation(dataingestionartifact, datavalidationconfig)
+        logging.info("Initiate the data validation")
+        datavalidationartifact = data_validation.initiate_data_validation()
+        logging.info("Data Validation Completed")
+        print(datavalidationartifact)
 
     except Exception as e:
            # Raise custom exception with error details
--- phishnet/utils/main_utils/utils.py
+++ phishnet/utils/main_utils/utils.py
@@ -1,14 +1,28 @@
-import yaml
-from phishnet.exception.exception import PhishnetException
-from phishnet.logging.logger import logging
-import os, sys
-import numpy as np
+import os
+import sys
 import dill
+import yaml
 import pickle
+import numpy as np
+from phishnet.logging.logger import logging
+from phishnet.exception.exception import PhishnetException
 
 def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
             return yaml.safe_load(yaml_file)
+    
     except Exception as e:
         raise PhishnetException(e, sys) from e
+
+def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+    try:
+        if replace:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "w") as file:
+            yaml.dump(content, file)
+    
+    except Exception as e:
+        raise PhishnetException(e, sys)
