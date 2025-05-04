d7f3806113b1cc67ac79e7cc34a8a93f519494ff
Suman Mondal <suman.mandal1020@gmail.com>
2025-05-04 13:49:19 +0530

    add func to save numpy array data and object file

--- phishnet/utils/main_utils/utils.py
+++ phishnet/utils/main_utils/utils.py
@@ -7,6 +7,7 @@ import numpy as np
 from phishnet.logging.logger import logging
 from phishnet.exception.exception import PhishnetException
 
+
 def read_yaml_file(file_path: str) -> dict:
     try:
         with open(file_path, "rb") as yaml_file:
@@ -26,3 +27,29 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
     
     except Exception as e:
         raise PhishnetException(e, sys)
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            np.save(file_obj, array)
+
+    except Exception as e:
+        raise PhishnetException(e, sys) from e
+    
+def save_object(file_path: str, obj: object) -> None:
+    try:
+        logging.info("Entered the save_object method of MainUtils class")
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        logging.info("Exited the save_object method of MainUtils class")
+    
+    except Exception as e:
+        raise PhishnetException(e, sys) from e
