54a9cceaef9ddd123c699ca61238ca4493829e83
Suman Mondal <suman.mandal1020@gmail.com>
2025-05-04 23:29:33 +0530

    feat: made data transformation component

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 from phishnet.logging.logger import logging
 from phishnet.exception.exception import PhishnetException
-from phishnet.components.data_validation import DataValidation
 from phishnet.components.data_ingestion import DataIngestion
-from phishnet.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from phishnet.components.data_validation import DataValidation
+from phishnet.components.data_transformation import DataTransformation
+from phishnet.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from phishnet.entity.config_entity import TrainingPipelineConfig
 
 if __name__=='__main__':
@@ -11,20 +12,29 @@ if __name__=='__main__':
         trainingpipelineconfig = TrainingPipelineConfig()
         
         # data ingestion
+        logging.info("Initiate Data Ingestion")
         dataingestionconfig = DataIngestionConfig(trainingpipelineconfig)
         data_ingestion = DataIngestion(dataingestionconfig)
-        logging.info("Initiate the data ingestion")
         dataingestionartifact = data_ingestion.initiate_data_ingestion()
         print(dataingestionartifact)
         logging.info("Data Initiation Complete")
 
         # data validation
+        logging.info("Initiate Data Validation")
         datavalidationconfig = DataValidationConfig(trainingpipelineconfig)
         data_validation = DataValidation(dataingestionartifact, datavalidationconfig)
-        logging.info("Initiate the data validation")
         datavalidationartifact = data_validation.initiate_data_validation()
-        logging.info("Data Validation Completed")
         print(datavalidationartifact)
+        logging.info("Data Validation Completed")
+
+        # data transformation
+        logging.info("Initiate Data Transformation")
+        datatransformationconfig = DataTransformationConfig(trainingpipelineconfig)
+        data_transformation = DataTransformation(datavalidationartifact, datatransformationconfig)
+        datatransformationartifact = data_transformation.initiate_data_transformation()
+        print(datatransformationartifact)
+        logging.info("Data Tranformation Completed")
+
 
     except Exception as e:
            # Raise custom exception with error details
---
+++ phishnet/components/data_transformation.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+from sklearn.impute import KNNImputer
+from sklearn.pipeline import Pipeline
+
+from phishnet.constant.training_pipeline import TARGET_COLUMN
+from phishnet.constant.training_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
+
+from phishnet.entity.artifact_entity import (
+    DataTransformationArtifact,
+    DataValidationArtifact
+)
+
+from phishnet.entity.config_entity import DataTransformationConfig
+from phishnet.exception.exception import PhishnetException
+from phishnet.logging.logger import logging
+from phishnet.utils.main_utils.utils import save_numpy_array_data, save_object
+
+
+class DataTransformation:
+    def __init__(self, data_validation_artifact:DataValidationArtifact,
+                 data_transformation_config:DataTransformationConfig):
+        try:
+            self.data_validation_artifact:DataValidationArtifact = data_validation_artifact
+            self.data_transformation_config:DataTransformationConfig = data_transformation_config
+
+        except Exception as e:
+            raise PhishnetException(e, sys)
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise PhishnetException(e, sys)
+
+    def get_data_transformer_object(cls) -> Pipeline:
+        """
+        It initialises a KNNImputer object with the parameters specified in the training_pipeline.py file and returns a Pipeline object with the KNNImputer object as the first step.
+
+        Args:
+            cls: DataTransformation
+        
+        Returns:
+            A Pipeline object
+        """
+        logging.info(
+            "Entered get_data_transformer_object method of Transformation class"
+        )
+        try:
+            imputer:KNNImputer = KNNImputer(**DATA_TRANSFORMATION_IMPUTER_PARAMS)
+            logging.info(f"Initialise KNNImputer with {DATA_TRANSFORMATION_IMPUTER_PARAMS}")
+
+            processor:Pipeline = Pipeline([("imputer",imputer)])
+
+            return processor
+        
+        except Exception as e:
+            raise PhishnetException(e, sys)
+
+
+
+    def initiate_data_transformation(self) -> DataTransformationArtifact:
+        logging.info("Entered initiate_data_transformation method of DataTransformation class")
+
+        try:
+            logging.info("Starting data transformation")    
+            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+
+            # Training dataframe
+            input_feature_train_df = train_df.drop(columns = [TARGET_COLUMN], axis=1)
+            target_feature_train_df = train_df[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
+
+            # Testing dataframe
+            input_feature_test_df = test_df.drop(columns = [TARGET_COLUMN], axis=1)
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(-1, 0)
+
+            prepocessor = self.get_data_transformer_object()
+            prepocessor_object = prepocessor.fit(input_feature_train_df)
+            transformed_input_train_feature = prepocessor_object.transform(input_feature_train_df)
+            transformed_input_test_feature = prepocessor_object.transform(input_feature_test_df)
+
+            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df)]
+            test_arr = np.c_[transformed_input_test_feature, np.array(target_feature_test_df)]
+
+            # Save numpy array data
+            save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr)
+            save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
+            save_object(self.data_transformation_config.transformed_object_file_path, prepocessor_object)
+
+            # Preparing artifacts
+            data_transformation_artifact = DataTransformationArtifact(
+                transformed_object_file_path = self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path = self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path = self.data_transformation_config.transformed_test_file_path
+            )
+
+            return data_transformation_artifact
+        
+        except Exception as e:
+            raise PhishnetException(e, sys)    
